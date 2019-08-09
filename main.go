cd19937dcf7ed1b7f618868095433aba4de2e013
jlccs <jlccskw@gmail.com>
2019-08-09 19:04:17 +0800

    init

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		AppID:  "wxa0d453600377b58a",
 		Secret: "46d7252c5e8b807ae415e165490dd4e7",
 	}
-	code := "071FY5U40Yl4bD1x9UW40ddKT40FY5UR"
+	code := "071FY5U40Yl4bD1x9UW40ddKT40FY5U"
 	loginData, err := w.WexLogin(code)
 	fmt.Println("data:", loginData)
 	//fmt.Println("data.Openid:", loginData.Openid)
